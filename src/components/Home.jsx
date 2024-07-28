25d939ea6d1b71f5f19a50b3d894928e0aec9340
emily020kim <emily020kim@gmail.com>
2024-07-28 14:15:15 -0700

    header animation & body cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import SplitTextJS from 'split-text-js';
 
 import Navbar from './Navbar';
 
@@ -10,12 +13,86 @@ const Wrapper = styled.div`
   padding-top: 80px;
 `;
 
+const Header = styled.div`
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  margin: 100px 0 200px 0;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 80px;
+  font-weight: 700;
+  align-content: center;
+  color: #4c00b0;
+  margin: 0;
+  line-height: 0;
+  letter-spacing: 2px;
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: center;
+  border: 3px solid #4c00b0;
+  border-radius: 15px;
+  padding: 20px 30px;
+  margin-bottom: 10px;
+`;
+
 const Home = () => {
+  const textWrapperRef = useRef(null);
+
+  useEffect(() => {
+    const titles = gsap.utils.toArray('.text-wrapper h1');
+    const tl = gsap.timeline({repeat: -1});
+    titles.forEach(title => {
+      const splitTitle = new SplitTextJS(title, { type: "chars" });
+
+      tl
+        .from(splitTitle.chars, {
+          opacity: 0,
+          y: 80,
+          rotateX: -90,
+          stagger: 0.02,
+        })
+        .to(
+          splitTitle.chars,
+          {
+            opacity: 0,
+            y: -80,
+            rotateX: 90,
+            stagger: 0.02,
+          },
+          "+=1"
+        );
+    });
+  }, []);
+
   return (
     <Wrapper>
       <Navbar />
-      <p>Home</p>
-      <p>seeing if it works</p>
+      <Header>
+        <div className="text-wrapper" ref={textWrapperRef}>
+          <HeaderText>Emily Kim</HeaderText>
+          <HeaderText>Developer</HeaderText>
+          <HeaderText>Art Lover</HeaderText>
+        </div>
+      </Header>
+      <Body>
+        <Card>content</Card>
+        <Card>content</Card>
+        <Card>content</Card>
+      </Body>
+
     </Wrapper>
   );
 };
